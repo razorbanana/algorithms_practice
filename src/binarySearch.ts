cdbe5426493e7c4196ebd6772c1410d57ab2ecf3
Stanislav Stefiuk <stanislav.stefyuk2@gmail.com>
2026-01-28 18:52:46 +0200

    feat: Binary Search is implemented

---
+++ src/binarySearch.ts
@@ -0,0 +1,25 @@
+const BinarySearch: <T>(array: T[], target: T, eval_function: (item:T, target:T) => -1 | 0 | 1) => number | null = (array, target, eval_function) => {
+    let lowest = 0
+    let highest = array.length - 1
+    let middle = -1
+    let eval_result: -1 | 0 | 1 
+    while (lowest <= highest){
+        middle = Math.floor((lowest + highest) / 2);
+        if (middle !== undefined) {
+            eval_result = eval_function(array[middle]!, target);
+        }else{
+            return null
+        }
+
+        if (eval_result === 1){
+            highest = middle - 1
+        }else if(eval_result === 0){
+            return middle
+        }else{
+            lowest = middle + 1
+        }
+    }
+    return null
+}
+
+export default BinarySearch
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+import BinarySearch from "./binarySearch.ts"
+
+const eval_function: (item:number, target:number) => -1 | 0 | 1 = (item, target) => {
+    if (item > target){
+        return 1
+    }else if (item < target){
+        return -1
+    }else{
+        return 0
+    }
+}
+
+const array = [1,2,5,6,7,8]
+const targets = [1,2,8,9]
+const results: Array<number|null> = []
+
+
+targets.forEach(target => {
+    results.push(BinarySearch<number>(array, target, eval_function))
+})
+
+results.map(result => console.log(result))
